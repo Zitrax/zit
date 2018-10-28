127a9c3e4d6e4be37686179291ffc28efee30202
Daniel Bengtsson <daniel@bengtssons.info>
2018-10-29 00:05:16 +0100

    Add afl target
    Fuzzed decoding with 6 processes for about 2 hours, no crash detected.

---
+++ src/main_afl.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include "bencode.h"
+
+int main(int argc, char** argv) {
+  if (argc > 1) {
+    std::ifstream fin;
+    fin.open(argv[1]);
+    // FIXME: Read from file
+    return 1;
+  } else {
+    // FIXME: check __AFL_LOOP
+    for (std::string line; std::getline(std::cin, line);) {
+        try {
+            bencode::decode(line);
+        } catch (const std::exception&){
+            // This is fine
+        }
+    }
+    return 0;
+  }
+}
