4d9ffff932972552e8e736d9668baf5a266a810c
Daniel Bengtsson <daniel@bengtssons.info>
2018-11-04 20:26:01 +0100

    Fixed up read_file

--- src/file_utils.h
+++ src/file_utils.h
@@ -7,12 +7,16 @@
 namespace zit {
 
 /**
- * @param file_name
+ * Read a file to a string.
+ *
+ * @param file_name path to file to read
+ *
+ * @throws if the file could not be read. Note that it also throws if the file
+ *   is empty.
  */
 inline auto read_file(const std::filesystem::path& file_name) {
   std::ifstream file_stream{file_name};
   file_stream.exceptions(std::ifstream::failbit);
-  file_stream.clear();
   std::ostringstream str_stream{};
   file_stream >> str_stream.rdbuf();
   return str_stream.str();
