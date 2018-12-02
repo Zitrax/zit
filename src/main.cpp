a0797b41c03c0762d5437fbfbe02434c675482a6
Daniel Bengtsson <daniel@bengtssons.info>
2018-12-02 19:18:46 +0100

    Update main with working test tracker request

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,14 @@ int main() {
     cout << torrent << "\n";
 
     zit::Url url(torrent.announce());
+    url.add_param("info_hash=" + zit::Net::url_encode(torrent.info_hash()));
+    url.add_param("peer_id=abcdefghijklmnopqrst");  // FIXME: Use proper id
+    url.add_param("port=6881");                     // FIXME: configure this
+    url.add_param("uploaded=0");
+    url.add_param("downloaded=0");
+    url.add_param("left=0");
+    url.add_param("event=started");
+    url.add_param("compact=1");  // TODO: Look up what this really means
     cout << url;
 
     // Net
@@ -54,7 +62,8 @@ int main() {
     auto[headers, body] = net.http_get(url);
     cout << "=====HEADER=====\n"
          << headers << "\n=====BODY=====\n"
-         << body << "\n";
+         << decode(body) << "\n";
+
   } catch (const exception& e) {
     print_exception(e);
     return 1;
