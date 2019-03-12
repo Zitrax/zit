bfb9be4b1a0d191c2b3544ea92be943b1e637e95
Daniel Bengtsson <daniel@bengtssons.info>
2019-03-12 21:47:44 +0100

    Add logger when testing to avoid crash

---
+++ tests/test_main.cpp
@@ -0,0 +1,12 @@
+// -*- mode:c++; c-basic-offset : 2; -*-
+#include "gtest/gtest.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+int main(int argc, char** argv) {
+  // Needed since the code assumes a global log object
+  auto console = spdlog::stdout_color_mt("console");
+
+  // Run the tests
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
