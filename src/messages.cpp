84685e0f411323cbf00fe8ca59d7ec0bdca7509e
Daniel Bengtsson <daniel@bengtssons.info>
2021-03-14 17:28:02 +0100

    Some notes about unsupported messages

--- src/messages.cpp
+++ src/messages.cpp
@@ -263,7 +263,15 @@ size_t Message::parse(PeerConnection& connection) {
           peer.request(index, begin, length);
           return len + 4;
         }
+        // Currently unsupported messages:
+
+        // Cancel is supposed to be used at the endgame to speed up the last
+        // pieces.
         case peer_wire_id::CANCEL:
+        // The port message is sent by newer versions of the Mainline that
+        // implements a DHT tracker. The listen port is the port this peer's DHT
+        // node is listening on. This peer should be inserted in the local
+        // routing table (if DHT tracker is supported).
         case peer_wire_id::PORT:
         case peer_wire_id::UNKNOWN:
           break;
