220cb13a4ae18d56bfc91d681dbd2154788b2e19
Daniel Bengtsson <daniel@bengtssons.info>
2022-09-04 14:52:54 +0200

    Fix test net.chunkedTransfer (SNI)
    This test now need SNI, apparently it not
    earlier so something changed server side.

--- src/net.cpp
+++ src/net.cpp
@@ -241,6 +241,13 @@ static std::tuple<std::string, std::string> httpsGet(const Url& url) {
   // Perform SSL handshake and verify the remote host's
   // certificate.
   sock.set_verify_mode(ssl::verify_peer);
+
+  // Without this some servers fail with "handshake: sslv3 alert handshake failure"
+  // These servers rely on SNI (Server Name Indication)
+  if(!SSL_set_tlsext_host_name(sock.native_handle(), url.host().c_str())) {
+    spdlog::get("console")->warn("Could not set host name for SNI");
+  }
+
   sock.set_verify_callback(
       [&url](bool preverified, asio::ssl::verify_context& ctx_) {
         if (!preverified) {
