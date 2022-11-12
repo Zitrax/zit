cca455484571ab977adadeb3dcf11406fc5bd65d
Daniel Bengtsson <daniel@bengtssons.info>
2022-11-12 12:23:20 +0100

    Change the initiate_peer_connections default #38

--- tests/test_global_config.cpp
+++ tests/test_global_config.cpp
@@ -19,7 +19,7 @@ TEST_F(FileConfigTest, EmptyFile) {
   const auto config_file = tmp_dir() / ".zit";
   write_file(config_file, "");
   const auto config = FileConfig{config_file};
-  EXPECT_TRUE(config.get(BoolSetting::INITIATE_PEER_CONNECTIONS));
+  EXPECT_FALSE(config.get(BoolSetting::INITIATE_PEER_CONNECTIONS));
   EXPECT_EQ(config.get(IntSetting::LISTENING_PORT), 20001);
   EXPECT_EQ(config.get(IntSetting::CONNECTION_PORT), 20000);
 }
@@ -28,16 +28,16 @@ TEST_F(FileConfigTest, InvalidFile) {
   const auto config_file = tmp_dir() / ".zit";
   write_file(config_file, "foo=bar\nlistening_port=nan");
   const auto config = FileConfig{config_file};
-  EXPECT_TRUE(config.get(BoolSetting::INITIATE_PEER_CONNECTIONS));
+  EXPECT_FALSE(config.get(BoolSetting::INITIATE_PEER_CONNECTIONS));
 }
 
 TEST_F(FileConfigTest, CorrectFile) {
   const auto config_file = tmp_dir() / ".zit";
   write_file(config_file,
-             "initiate_peer_connections=false\nlistening_port=123\nconnection_"
+             "initiate_peer_connections=true\nlistening_port=123\nconnection_"
              "port=321");
   const auto config = FileConfig{config_file};
-  EXPECT_FALSE(config.get(BoolSetting::INITIATE_PEER_CONNECTIONS));
+  EXPECT_TRUE(config.get(BoolSetting::INITIATE_PEER_CONNECTIONS));
   EXPECT_EQ(config.get(IntSetting::LISTENING_PORT), 123);
   EXPECT_EQ(config.get(IntSetting::CONNECTION_PORT), 321);
 }
