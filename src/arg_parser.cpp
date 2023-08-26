e54490ac942ada92efda4a9ce592fc6126a80508
Daniel Bengtsson <daniel@bengtssons.info>
2023-08-26 18:30:47 +0200

    Use builder pattern for ArgParser

--- src/arg_parser.cpp
+++ src/arg_parser.cpp
@@ -1,7 +1,6 @@
 // -*- mode:c++; c-basic-offset : 2; -*-
 #include "arg_parser.hpp"
 
-#include "string_utils.hpp"
 #include "types.hpp"
 
 #ifndef _MSC_VER
@@ -11,101 +10,30 @@
 #include <algorithm>
 #include <cerrno>
 #include <cstddef>
-#include <optional>
 #include <sstream>
 #include <stdexcept>
 #include <string>
-#include <string_view>
 #include <vector>
 
 using namespace std;
 
 namespace zit {
 
-template <typename T>
-void ArgParser::add_option(const string& options,
-                           const optional<T>& def,
-                           const string& help,
-                           T& dst,
-                           bool required,
-                           bool help_option) {
-  if (def) {
-    dst = def.value();
-  }
-  const auto argType = [] {
-    if constexpr (std::is_same_v<T, bool>) {
-      return Type::BOOL;
-    } else if (std::is_same_v<T, float>) {
-      return Type::FLOAT;
-    } else if (std::is_same_v<T, int>) {
-      return Type::INT;
-    } else if (std::is_same_v<T, unsigned>) {
-      return Type::UINT;
-    } else if (std::is_same_v<T, string>) {
-      return Type::STRING;
-    }
-  }();
-
-  bool alias = false;
-  for (const auto& option : split(options, ",")) {
-    if (ranges::find_if(m_options, [&](const auto& a) {
-          return a->option() == option;
-        }) != m_options.end()) {
-      throw runtime_error(fmt::format("Duplicate option '{}' added", option));
-    }
-
-    if (!alias) {
-      if (ranges::find_if(m_options, [&](const auto& a) {
-            return a->dst() == &dst;
-          }) != m_options.end()) {
-        throw runtime_error(
-            fmt::format("Duplicate value reference for '{}' added", option));
-      }
-    }
-
-    m_options.emplace_back(std::make_unique<Arg<T>>(option, alias ? "〃" : help,
-                                                    argType, dst, required));
-    m_options.back()->set_help_arg(help_option);
-    alias = true;
-  }
+ArgParser::ConstArgIterator ArgParser::find(const std::string& option) const {
+  return std::ranges::find_if(m_options, [&](const auto& a) {
+    return a->get_option() == option || a->get_aliases().contains(option);
+  });
 }
 
-void ArgParser::add_help_option(const std::string& options,
-                                const std::string& help,
-                                bool& dst) {
-  add_option(options, {false}, help, dst, false, true);
+ArgParser::ArgIterator ArgParser::find(const std::string& option) {
+  return std::ranges::find_if(m_options, [&](const auto& a) {
+    return a->get_option() == option || a->get_aliases().contains(option);
+  });
 }
 
-template void ArgParser::add_option<bool>(const string&,
-                                          const optional<bool>&,
-                                          const string&,
-                                          bool&,
-                                          bool,
-                                          bool);
-template void ArgParser::add_option<int>(const string&,
-                                         const optional<int>&,
-                                         const string&,
-                                         int&,
-                                         bool,
-                                         bool);
-template void ArgParser::add_option<unsigned>(const string&,
-                                              const optional<unsigned>&,
-                                              const string&,
-                                              unsigned&,
-                                              bool,
-                                              bool);
-template void ArgParser::add_option<float>(const string&,
-                                           const optional<float>&,
-                                           const string&,
-                                           float&,
-                                           bool,
-                                           bool);
-template void ArgParser::add_option<string>(const string&,
-                                            const optional<string>&,
-                                            const string&,
-                                            string&,
-                                            bool,
-                                            bool);
+bool ArgParser::has_option(const std::string& option) const {
+  return find(option) != m_options.end();
+}
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays)
 void ArgParser::parse(const std::vector<std::string>& argv) {
@@ -115,15 +43,14 @@ void ArgParser::parse(const std::vector<std::string>& argv) {
   m_parsed = true;
   for (size_t i = 1; i < argv.size(); i++) {
     // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    std::string_view name = argv[i];
-    auto m = ranges::find_if(
-        m_options, [&](const auto& a) { return a->option() == name; });
+    const std::string& name = argv[i];
+    auto m = find(name);
     if (m == m_options.end()) {
       throw runtime_error(fmt::format("Unknown argument: {}", name));
     }
     auto& arg = *m;
 
-    if (arg->type() == Type::BOOL) {
+    if (arg->get_type() == Type::BOOL) {
       as<bool>(arg).set_dst(true);
     } else {
       // Read next arg
@@ -133,7 +60,7 @@ void ArgParser::parse(const std::vector<std::string>& argv) {
       // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
       const auto& val = argv[i++ + 1];
 
-      switch (arg->type()) {
+      switch (arg->get_type()) {
         case Type::FLOAT: {
           const float fval = std::stof(val, nullptr);
           if (errno == ERANGE) {
@@ -171,14 +98,15 @@ void ArgParser::parse(const std::vector<std::string>& argv) {
   }
 
   // Check if all options got values
-  auto missing_required_arg = ranges::find_if(
-      m_options, [](const auto& o) { return !o->provided() && o->required(); });
+  auto missing_required_arg = ranges::find_if(m_options, [](const auto& o) {
+    return !o->is_provided() && o->is_required();
+  });
   if (missing_required_arg != m_options.end()) {
     if (ranges::none_of(m_options, [](const auto& o) {
-          return o->help_arg() && o->provided();
+          return o->is_help_arg() && o->is_provided();
         })) {
       throw runtime_error(fmt::format("Required option '{}' not provided",
-                                      (*missing_required_arg)->option()));
+                                      (*missing_required_arg)->get_option()));
     }
   }
 }
@@ -188,16 +116,17 @@ std::string ArgParser::usage() {
   ss << "Usage:\n\n" << m_desc << "\n\n";
 
   auto it = ranges::max_element(m_options, [](const auto& a, const auto& b) {
-    return a->option().size() < b->option().size();
+    return a->get_option().size() < b->get_option().size();
   });
   if (it == m_options.end()) {
     return ss.str();
   }
-  const auto width = (*it)->option().size();
+  const auto width = (*it)->get_option().size();
 
   for (const auto& option : m_options) {
-    ss << fmt::format("  {:{}}    {} {}\n", option->option(), width,
-                      option->help(), option->required() ? "(required)"s : ""s);
+    ss << fmt::format("  {:{}}    {} {}\n", option->get_option(), width,
+                      option->get_help(),
+                      option->is_required() ? "(required)"s : ""s);
   }
   return ss.str();
 }
--- src/arg_parser.hpp
+++ src/arg_parser.hpp
@@ -1,9 +1,14 @@
 // -*- mode:c++; c-basic-offset : 2; -*-
 #pragma once
 
+#include <fmt/core.h>
+#include <algorithm>
 #include <any>
+#include <iterator>
 #include <memory>
 #include <optional>
+#include <set>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -13,34 +18,157 @@ namespace zit {
  * Simple argument parser
  */
 class ArgParser {
+ private:
+  enum class Type { INT, UINT, FLOAT, STRING, BOOL };
+
+  struct BaseArg {
+    explicit BaseArg(std::string option, Type type)
+        : m_option(std::move(option)), m_type(type) {}
+
+    virtual ~BaseArg() = default;
+    virtual void* dst() = 0;
+
+    [[nodiscard]] auto get_option() const { return m_option; }
+    [[nodiscard]] const auto& get_aliases() const { return m_aliases; }
+    [[nodiscard]] auto get_help() const { return m_help; }
+    [[nodiscard]] auto get_type() const { return m_type; }
+    [[nodiscard]] auto is_provided() const { return m_provided; }
+    [[nodiscard]] auto is_required() const { return m_required; }
+    [[nodiscard]] auto is_help_arg() const { return m_help_arg; }
+
+    void set_help_arg(bool help_arg) { m_help_arg = help_arg; }
+    void set_provided(bool provided) { m_provided = provided; }
+
+   protected:
+    std::string m_option;
+    std::string m_help{};
+    std::set<std::string> m_aliases{};
+    Type m_type;
+    bool m_provided = false;
+    bool m_required = false;
+    bool m_help_arg = false;
+  };
+
+  template <typename T>
+  static constexpr auto typeEnum() {
+    if constexpr (std::is_same_v<T, bool>) {
+      return Type::BOOL;
+    } else if (std::is_same_v<T, float>) {
+      return Type::FLOAT;
+    } else if (std::is_same_v<T, int>) {
+      return Type::INT;
+    } else if (std::is_same_v<T, unsigned>) {
+      return Type::UINT;
+    } else if (std::is_same_v<T, std::string>) {
+      return Type::STRING;
+    }
+  }
+
+  using ConstArgIterator =
+      std::vector<std::unique_ptr<BaseArg>>::const_iterator;
+  using ArgIterator = std::vector<std::unique_ptr<BaseArg>>::iterator;
+
+  template <typename T>
+  struct Arg : public BaseArg {
+   public:
+    explicit Arg(std::string option)
+        : BaseArg(std::move(option), ArgParser::typeEnum<T>()) {
+      if constexpr (std::is_same_v<T, bool>) {
+        // Special for bool - a non-provided arg always default to false
+        m_dst = false;
+      }
+    }
+
+    void* dst() override { return &m_dst; }
+
+    [[nodiscard]] auto dst() const { return m_dst; }
+    void set_dst(T t) { m_dst = std::move(t); }
+
+    auto& help(const std::string& help) {
+      m_help = help;
+      return *this;
+    }
+
+    auto& required() {
+      m_required = true;
+      return *this;
+    }
+
+    auto& default_value(T t) {
+      m_dst = std::move(t);
+      return *this;
+    }
+
+    auto& help_arg() {
+      m_help_arg = true;
+      return *this;
+    }
+
+    auto& aliases(const std::set<std::string>& aliases) {
+      m_aliases = aliases;
+      return *this;
+    }
+
+   private:
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
+    std::optional<T> m_dst{};
+  };
+
+  template <typename T>
+  auto& as(std::unique_ptr<ArgParser::BaseArg>& arg) {
+    return dynamic_cast<Arg<T>&>(*arg);
+  }
+
+  [[nodiscard]] ConstArgIterator find(const std::string& option) const;
+  [[nodiscard]] ArgIterator find(const std::string& option);
+
+  [[nodiscard]] bool has_option(const std::string& option) const;
+
+  std::string m_desc;
+  std::vector<std::unique_ptr<BaseArg>> m_options{};
+  bool m_parsed = false;
+
  public:
   explicit ArgParser(std::string desc) : m_desc(std::move(desc)) {}
 
-  /**
-   * Add an option to be parsed by parse().
-   * @param options One arg like "--test", or a comma separated list like
-   *   "--test,-t" for providing aliases.
-   * @param def Optional default value.
-   * @param help Descriptive help text.
-   * @param dst Reference to variable to be filled with the option value.
-   * @param required True if the option is required.
-   * @param help_option True if the option is an help argument. See @ref
-   *   add_help_option.
-   */
   template <typename T>
-  void add_option(const std::string& options,
-                  const std::optional<T>& def,
-                  const std::string& help,
-                  T& dst,
-                  bool required = false,
-                  bool help_option = false);
+  Arg<T>& add_option(std::string option) {
+    if (has_option(option)) {
+      throw std::runtime_error(
+          fmt::format("Duplicate option '{}' added", option));
+    }
 
-  /**
-   * Add specific help option. This can be passed alone without required flags.
-   */
-  void add_help_option(const std::string& option,
-                       const std::string& help,
-                       bool& dst);
+    m_options.emplace_back(std::make_unique<Arg<T>>(option));
+    return static_cast<Arg<T>&>(*m_options.back());
+  }
+
+  template <typename T>
+  [[nodiscard]] T get(const std::string& option) const {
+    const auto match = find(option);
+    if (match == m_options.end()) {
+      throw std::runtime_error("No option: " + option);
+    }
+    const auto* opt = dynamic_cast<const Arg<T>*>(match->get());
+    if (!opt) {
+      throw std::runtime_error("Invalid type for option: " + option);
+    }
+    if (opt->is_required() && !opt->is_provided()) {
+      throw std::runtime_error("No value for required option: " + option);
+    }
+    auto dst = opt->dst();
+    if (dst.has_value()) {
+      return dst.value();
+    }
+    throw std::runtime_error("No value provided for option: " + option);
+  }
+
+  [[nodiscard]] bool is_provided(const std::string& option) const {
+    const auto match = find(option);
+    if (match == m_options.end()) {
+      throw std::runtime_error("No option: " + option);
+    }
+    return match->get()->is_provided();
+  }
 
   /**
    * Parse arguments for options provided by add_option.
@@ -51,64 +179,6 @@ class ArgParser {
    * Get help info string for all available options.
    */
   std::string usage();
-
- private:
-  enum class Type { INT, UINT, FLOAT, STRING, BOOL };
-
-  struct BaseArg {
-    BaseArg(std::string option, std::string help, Type type, bool required)
-        : m_option(std::move(option)),
-          m_help(std::move(help)),
-          m_type(type),
-          m_required(required) {}
-    virtual ~BaseArg() = default;
-    virtual void* dst() = 0;
-
-    [[nodiscard]] auto option() const { return m_option; }
-    [[nodiscard]] auto help() const { return m_help; }
-    [[nodiscard]] auto type() const { return m_type; }
-    [[nodiscard]] auto provided() const { return m_provided; }
-    [[nodiscard]] auto required() const { return m_required; }
-    [[nodiscard]] auto help_arg() const { return m_help_arg; }
-
-    void set_help_arg(bool help_arg) { m_help_arg = help_arg; }
-    void set_provided(bool provided) { m_provided = provided; }
-
-   private:
-    std::string m_option;
-    std::string m_help;
-    Type m_type;
-    bool m_provided = false;
-    bool m_required = false;
-    bool m_help_arg = false;
-  };
-
-  template <typename T>
-  struct Arg : public BaseArg {
-   public:
-    Arg(const std::string& option,
-        const std::string& help,
-        Type type,
-        T& dst,
-        bool required)
-        : BaseArg(option, help, type, required), m_dst(dst) {}
-    void* dst() override { return &m_dst; }
-
-    [[nodiscard]] auto dst() const { return m_dst; }
-    void set_dst(T t) { m_dst = std::move(t); }
-
-   private:
-   // NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
-    T& m_dst{};
-  };
-
-  template <typename T>
-  auto& as(std::unique_ptr<ArgParser::BaseArg>& arg) {
-    return dynamic_cast<Arg<T>&>(*arg);
-  }
-
-  std::string m_desc;
-  std::vector<std::unique_ptr<BaseArg>> m_options{};
-  bool m_parsed = false;
 };
+
 }  // namespace zit
