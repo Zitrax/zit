2c2951cc1ccc5d58d2e68f4f7444825889335c34
Daniel Bengtsson <daniel@bengtssons.info>
2023-10-08 15:21:16 +0200

    Use fewer decimals in Timer

--- src/timer.hpp
+++ src/timer.hpp
@@ -7,18 +7,18 @@ namespace zit {
 
 class Timer {
  public:
-  explicit Timer(std::string name)
-      : m_name(std::move(name)), m_start(std::chrono::system_clock::now()) {}
+  explicit Timer(std::string name) : m_name(std::move(name)) {}
 
   ~Timer() {
     const std::chrono::duration<double> total =
         std::chrono::system_clock::now() - m_start;
-    logger()->info("{} seconds spent on {}", total.count(), m_name);
+    logger()->info("{:.3f} seconds spent on {}", total.count(), m_name);
   }
 
  private:
   std::string m_name;
-  std::chrono::time_point<std::chrono::system_clock> m_start;
+  std::chrono::time_point<std::chrono::system_clock> m_start{
+      std::chrono::system_clock::now()};
 };
 
 }  // namespace zit
