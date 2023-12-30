16666fa3caf24fd53b08d2fe61454ca6a4d1335c
Daniel Bengtsson <daniel@bengtssons.info>
2023-12-30 12:46:33 +0100

    Cleaned up signal handling

--- src/socket_test/server.cpp
+++ src/socket_test/server.cpp
@@ -1,44 +1,16 @@
-#include <bits/chrono.h>
+
 #include <asio/awaitable.hpp>
 #include <asio/co_spawn.hpp>
 #include <asio/detached.hpp>
 #include <asio/io_context.hpp>
 #include <asio/ip/tcp.hpp>
+#include <asio/signal_set.hpp>
 #include <asio/use_awaitable.hpp>
 #include <csignal>
 #include <exception>
-#include <tuple>
 #include <vector>
 #include "formatters.hpp"  // NOLINT(misc-include-cleaner)
 #include "logger.hpp"
-
-// Can't use cstring currently - see
-// https://github.com/llvm/llvm-project/issues/76567
-// NOLINTNEXTLINE(modernize-deprecated-headers)
-#include <string.h>
-
-namespace {
-
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
-bool aborted = false;
-
-// ctrl-c linux signal handler
-void signal_handler(int signum) {
-  if (signum == SIGINT) {
-    zit::logger()->debug("Caught abort/ctrl-c");
-    aborted = true;
-  } else {
-    // NOLINTNEXTLINE(concurrency-mt-unsafe)
-    zit::logger()->info("Got signal '{}'", strsignal(signum));
-  }
-}
-
-// Register the signal handler
-void register_signal_handler() {
-  std::ignore = signal(SIGINT, signal_handler);
-}
-
-}  // namespace
 
 class Connection {
  public:
@@ -74,18 +46,17 @@ class Connection {
 };
 
 int main() {
-  using namespace std::chrono_literals;
   try {
-    // TODO: Apparently there is a simpler way by instructing asio to handle
-    // signals
-    register_signal_handler();
-    zit::logger()->info("Starting server. Press Ctrl-C to stop.");
     asio::io_context io_context;
+
+    // Ensuring ctrl-c shuts down cleanly
+    asio::signal_set signals(io_context, SIGINT, SIGTERM);
+    signals.async_wait([&io_context](auto, auto) { io_context.stop(); });
+
+    zit::logger()->info("Starting server. Press Ctrl-C to stop.");
     Connection connection(io_context);
     co_spawn(io_context, connection.listen(), asio::detached);
-    while (!aborted) {
-      io_context.run_for(100ms);
-    }
+    io_context.run();
     zit::logger()->info("Shutting down server");
   } catch (const std::exception& e) {
     zit::logger()->error("Exception: {}", e.what());
