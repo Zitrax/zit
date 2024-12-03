b2da7cbbad45250c8a39aae0cc5c343df58518dd
Daniel Bengtsson <daniel@bengtssons.info>
2024-12-03 15:32:32 +0100

    scope_guard.hpp: Fix review issues
    Add documentation and ensure exceptions are not escaping the destructor.

--- src/scope_guard.hpp
+++ src/scope_guard.hpp
@@ -1,17 +1,32 @@
 #pragma once
 
+#include "logger.hpp"
+
 namespace zit {
 
+/**
+ * ScopeGuard is a class that will call a function when it goes out of scope.
+ */
 class ScopeGuard {
  public:
   template <typename Callable>
-  explicit ScopeGuard(Callable&& onExit) : m_exit_fn(std::forward<Callable>(onExit)) {}
+  explicit ScopeGuard(Callable&& onExit)
+      : m_exit_fn(std::forward<Callable>(onExit)) {}
 
   ScopeGuard(const ScopeGuard&) = delete;
   ScopeGuard& operator=(const ScopeGuard&) = delete;
   ScopeGuard(ScopeGuard&& other) = delete;
 
-  ~ScopeGuard() { m_exit_fn(); }
+  ~ScopeGuard() {
+    try {
+      m_exit_fn();
+    } catch (...) {
+      try {
+        logger()->warn("Exception thrown from ScopeGuard");
+      } catch (...) {
+      }
+    }
+  }
 
  private:
   std::function<void()> m_exit_fn;
